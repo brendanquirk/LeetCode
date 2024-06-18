c761d878c9f910f2f10b1808e1cae8576cfbc64a
Brendan Quirk <43979769+brendanquirk@users.noreply.github.com>
2024-06-17 23:29:12 -0400

    [21] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 21-Merge-Two-Sorted-Lists.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+const mergeTwoLists = (list1, list2) => {
+    //create a new dummy starting point for our new merged linked list
+    const dummy = new ListNode(-Infinity)
+    
+    //create a ref to the dummy node so we can return this later
+    let prev = dummy
+
+    while (list1 && list2){
+        //check what value is greater to determine which list to merge from next
+        if (list1.val <= list2.val) {
+            //set pointer 
+            prev.next = list1
+            //increment prev to whatever list1 currently is in the loop
+            prev = list1
+            //increment list1 to its pointer
+            list1 = list1.next
+        } else {
+            //same steps but for list2
+            prev.next = list2
+            prev = list2
+            list2 = list2.next
+        }
+    }
+    //if list1 is empty merge in list2
+    if (!list1) prev.next = list2
+    //if list2 is empty merge in list1
+    if (!list2) prev.next = list1
+
+    //return everything after our dummy node
+    return dummy.next
+
+}
